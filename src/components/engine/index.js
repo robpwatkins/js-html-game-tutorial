c0f9d889d39123ec4e915bb6a00d15210299a190
robpwatkins <robpwatkins@gmail.com>
2020-08-22 16:42:25 -0500

    time grocery

--- src/components/engine/index.js
+++ src/components/engine/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import styles from './engine.module.scss';
+import { useEvent } from '../../hooks';
 
 export default function Engine() {
+    const handleKeyPress = (e) => {
+        if (e.key === ' ') {
+            console.log('hittin that spacebar, i see.');
+        }
+    };
+
+    useEvent('keyup', handleKeyPress);
+
     return (
         <div
             className={styles.container}
