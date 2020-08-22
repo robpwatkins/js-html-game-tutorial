3204fab58b2b8235c1a05859966f30830cabd7b6
robpwatkins <robpwatkins@gmail.com>
2020-08-22 16:33:07 -0500

    coo we got colors on the screen

---
+++ src/components/engine/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './engine.module.scss';
+
+export default function Engine() {
+    return (
+        <div
+            className={styles.container}
+        >
+            <span
+                className={styles.character}
+            />
+        </div>
+    );
+}
